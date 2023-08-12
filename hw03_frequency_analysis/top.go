09419c3d8e5c964de6ccebf17d07c4f9e4d33d89
Данил TruePi4 Матюхин <post7@intersvyaz.net>
2023-08-13 04:36:57 +0500

    hw03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,49 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type Word struct {
+	name  string
+	count int
+}
+
+func Top10(text string) []string {
+	wordCounted := []Word{}
+
+	if len(text) == 0 {
+		return nil
+	}
+
+	textFields := strings.Fields(text)
+
+	for _, name := range textFields {
+		found := false
+		for i := 0; i < len(wordCounted); i++ {
+			if wordCounted[i].name == name {
+				wordCounted[i].count++
+				found = true
+				break
+			}
+		}
+		if !found {
+			wordCounted = append(wordCounted, Word{name, 1})
+		}
+	}
+	sort.Slice(wordCounted, func(i, j int) bool {
+		return wordCounted[i].count > wordCounted[j].count ||
+			(wordCounted[i].count == wordCounted[j].count &&
+				strings.Compare(wordCounted[i].name, wordCounted[j].name) == -1)
+	})
+
+	result := make([]string, 0)
+	for _, item := range wordCounted {
+		result = append(result, item.name)
+	}
+	if len(result) < 10 {
+		return result
+	}
+	return result[:10]
 }
